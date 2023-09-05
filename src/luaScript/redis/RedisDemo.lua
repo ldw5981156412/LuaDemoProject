e6319bd06317c4bb2f497b1af884a1ce436dee76
liudawei <598156412@163.com>
2023-09-05 16:34:50 +0800

    实战

--- src/luaScript/redis/RedisDemo.lua
+++ src/luaScript/redis/RedisDemo.lua
@@ -2,18 +2,68 @@ local redis = require("resty.redis");
 local config = require("luaScript.module.config.redis-config");
 
 -- 设置超时时长
-local red = redis:new();
+local red = redis:new()
 -- 设置超时时长,单位为ms
-red:set_timeouts(config.timeout,config.timeout,config.timeout);
+red:set_timeouts(config.timeout,config.timeout,config.timeout)
 
 -- 连接服务器
-local ok,err = red:connect(config.host_name,config.port);
+local ok,err = red:connect(config.host_name,config.port)
 if not ok then
-    ngx.say("failed to connect: ",err);
+    ngx.say("failed to connect: ",err)
     return
 else
-    ngx.say("succeed to connect redis","<br>");
+    ngx.say("succeed to connect redis","<br>")
 end
 
+-- 设置值
+ok,err = red:set("dog","an animal");
+if not ok then
+    ngx.say("failed to set dog: ",err,"<br>")
+    return
+else
+    ngx.say("set dog ok","<br>")
+end
+-- 取值
+local res,err = red:get("dog")
+if not res or res == ngx.null then
+    ngx.say("failed to get dog:",err,"<br>")
+    return
+else
+    ngx.say("get dog ok:","<br>",res,"<br>")
+end
 
+-- 批量操作，减少网络IO
+red:init_pipeline()
+red:set("cat", "cat 1")
+red:set("horse", "horse 1")
+red:get("cat")
+red:get("horse")
+red:get("dog")
+local results, err = red:commit_pipeline()
+if not results then
+    ngx.say("failed to commit the pipelined requests: ", err)
+    return
+end
 
+for i, res in ipairs(results) do
+    if type(res) == "table" then
+        if res[1] == false then
+            ngx.say("failed to run command ", i, ": ", res[2], "<br>")
+        else
+            --处理表容器
+            ngx.say("succeed to run command ", i, ": ", res[i], "<br>")
+        end
+    else
+        -- 处理变量
+        ngx.say("succeed to run command ", i, ": ", res, "<br>")
+    end
+end
+
+--简单的关闭连接为:
+local ok, err = red:close()
+if not ok then
+    ngx.say("failed to close: ", err)
+    return
+else
+    ngx.say("succeed to close redis")
+end
